d7ca68f61e2ddee5e4661fe73eba43b36efb2fbf
hnulst@foxmail.com <hnulst@foxmail.com>
2022-04-17 17:04:38 +0800

    调整为相对目录

--- jump.py
+++ jump.py
@@ -1,21 +1,23 @@
 import math
+import os
 import subprocess
+import sys
 import time
 from multiprocessing import Process, Queue
 
 import cv2
 from matplotlib import pyplot as plt
 
-BASE_PATH = 'C:\\Users\\Jenny\\Desktop\\jump\\'
+BASE_PATH = sys.path[0]
 CAP = 'adb shell screencap -p //sdcard//jump.png'
 PULL = 'adb pull //sdcard//jump.png '
 LONG_CLICK = 'adb shell input swipe 500 500 510 510 '
-ALPHA = 1.37
+ALPHA = 1.07
 
 TM_METHOD = 'cv2.TM_CCOEFF'
 
 coor_queue = Queue()
-template = cv2.imread('C:\\Users\\Jenny\\Desktop\\jump\\ren.png', 0)
+template = cv2.imread(filename=os.path.join(BASE_PATH, 'ren.png'), flags=0)
 w, h = template.shape[::-1]
 
 fig = plt.figure()
@@ -30,7 +32,7 @@ def on_click(event):
     show()
 
 
-fig.canvas.mpl_connect('button_press_event', on_click)
+fig.canvas.mpl_connect(s='button_press_event', func=on_click)
 
 
 def coor_to_time(coor):
@@ -38,7 +40,7 @@ def coor_to_time(coor):
     (x1, y1) = coor[1]
     dist = math.sqrt((x0 - x1) * (x0 - x1) + (y0 - y1) * (y0 - y1))
     # print('dist = %.2f' % (dist))
-    return (int)(ALPHA * dist)
+    return int(ALPHA * dist)
 
 
 def jump(cq):
@@ -54,22 +56,22 @@ def jump(cq):
         idx += 1
 
 
-def match(img, template):
-    res = cv2.matchTemplate(img, template, eval(TM_METHOD))
+def match(img):
+    res = cv2.matchTemplate(image=img, templ=template, method=eval(TM_METHOD))
     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
     top_left = max_loc
     bottom_right = (top_left[0] + w, top_left[1] + h)
-    cv2.rectangle(img, top_left, bottom_right, 255, 2)
+    cv2.rectangle(img=img, pt1=top_left, pt2=bottom_right, color=255, thickness=2)
     x = top_left[0] + w / 2
     y = bottom_right[1] - h / 10
-    cv2.circle(img, ((int)(x), (int)(y)), 3, (0, 0, 255), -1)
+    cv2.circle(img=img, center=(int(x), int(y)), radius=3, color=(0, 0, 255), thickness=-1)
     print('from (%.2f, %.2f)' % (x, y))
     coor_queue.put((x, y))
 
 
 def show():
-    img = cv2.imread(BASE_PATH + '.png', 0)
-    match(img, template)
+    img = cv2.imread(filename=os.path.join(BASE_PATH, '.png'), flags=0)
+    match(img)
     plt.imshow(img, cmap='gray')
     plt.show()
 
@@ -78,8 +80,8 @@ def capture():
     time.sleep(3)
 
     run_cmd(CAP)  # screencap
-    print('capturing ' + '.png\n')
-    run_cmd(PULL + BASE_PATH + '.png')
+    print('capturing ' + os.path.join(BASE_PATH, '.png'))
+    run_cmd(PULL + os.path.join(BASE_PATH, '.png'))
 
 
 def run_cmd(cmd):
